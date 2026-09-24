75103c573f5ceb41a7373820d47da704462f7d11
sunnyraj <mrsj5714@gmail.com>
2026-09-24 09:24:42 +0530

    python problem find LCM of 2 numbers added

---
+++ Basics_Concepts/find_LCM.py
@@ -0,0 +1,10 @@
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+greater = max(num1, num2)
+
+while True:
+    if greater % num1 == 0 and greater % num2 == 0:
+        print("LCM =", greater)
+        break
+    greater += 1
